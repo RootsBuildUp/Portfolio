3cc91154444529536e25d5bd161f6f6271041d2d
RootsbuildUp <rashedfci994@gmail.com>
2022-08-13 02:07:14 +0600

    minimum info filup

---
+++ precache-manifest.7ab5ec7695c92ee57b2a5f4c2c51edf5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b97bed4e0c4a4b9bacff86a3410b1c0",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "97e02274851656e55b69",
+    "url": "/Portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/Portfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "910c9cc639c66d5006a4",
+    "url": "/Portfolio/static/css/main.88d33d83.chunk.css"
+  },
+  {
+    "revision": "1a7481c1416aff3c1179",
+    "url": "/Portfolio/static/js/2.e89b0d56.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/Portfolio/static/js/2.e89b0d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e02274851656e55b69",
+    "url": "/Portfolio/static/js/3.dd08fcab.chunk.js"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/Portfolio/static/js/4.09e78f1c.chunk.js"
+  },
+  {
+    "revision": "910c9cc639c66d5006a4",
+    "url": "/Portfolio/static/js/main.515b70f3.chunk.js"
+  },
+  {
+    "revision": "ea42b027d7caea14447a",
+    "url": "/Portfolio/static/js/runtime-main.07e9bb3e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4d5ddb8c3c8c5284fe4961ee5cfa933e",
+    "url": "/Portfolio/static/media/ban_logo.4d5ddb8c.svg"
+  },
+  {
+    "revision": "dc00e29676685236351334c6908fc2e7",
+    "url": "/Portfolio/static/media/city-bank.dc00e296.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/Portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "722c5710002814308caff02ffb294c0d",
+    "url": "/Portfolio/static/media/daffodil-international-university-logo.722c5710.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/Portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/Portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
